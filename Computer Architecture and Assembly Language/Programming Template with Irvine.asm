ab22c5ba594f3815f20cc4a6a8cb15a76a3989de
Yaseer Sabir <yaseersabir005@gmail.com>
2025-07-25 00:20:41 -0600

    pushing-files-from-downloadpage

---
+++ Computer Architecture and Assembly Language/Programming Template with Irvine.asm
@@ -0,0 +1,45 @@
+; **********************************************************************;
+; Program Name:   Template (Template.asm)				;
+; Program Description:							;
+; Author:								;
+; Creation Date:							;
+; Revisions: 								;
+; Date Last Modified:							;
+;***********************************************************************;
+
+INCLUDE C:\Irvine\Irvine32.inc
+INCLUDELIB C:\Irvine\Irvine32.lib
+
+.data
+; declare variables here
+
+; declare strings here
+
+; declare arrays here
+
+
+; **********************************************************************;
+; Functional description of the main program				;
+;	Inputs								;
+;	Outputs								;
+;	Registers used and associated purpose of each			;
+;	Memory locations use and associated purpose of each		;					
+;	Functional details						;
+; There should be a similar block prior to procedures, functions, or   ;
+;	otherwise major sections of code				;
+; **********************************************************************;
+
+.code
+main PROC
+
+    	; Write your code here.  
+	; Include non-syntax based comments for EACH line describing the 
+	;	contribution of the line to the overal functional goals
+	;	of the program.
+
+
+    INVOKE ExitProcess,0
+
+main ENDP
+
+END main
